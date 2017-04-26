a485648d47e4554d135eff8cad909ea567b7db91
Lalit Goyal <lgoyal06@gmail.com>
2017-04-26 21:59:56 +0530

    Initial logic to perform Predictive Parsing

--- src/main/java/com/lalit/compiler/phase/two/syntaxAnalyser/core/topdown/parser/PredictiveParsingAlgo.java
+++ src/main/java/com/lalit/compiler/phase/two/syntaxAnalyser/core/topdown/parser/PredictiveParsingAlgo.java
@@ -2,9 +2,29 @@ package com.lalit.compiler.phase.two.syntaxAnalyser.core.topdown.parser;
 
 import com.lalit.stack.api.StackViaLinkedList;
 
-//TODO 4 AM at 26th April 2017
+//TODO 4 AM at 27th April 2017 must
 
 public class PredictiveParsingAlgo {
 
-	StackViaLinkedList s = new StackViaLinkedList();
+	public static void main(String... s) {
+		StackViaLinkedList<String> stackOfSymbols = new StackViaLinkedList<>();
+
+		char[] incomingTokenBuffer = s[0].toCharArray();
+
+		// Push top Non terminal to start
+		stackOfSymbols.push("E");
+
+		while (stackOfSymbols.size() > 0) {
+			// terminal matched move pointer
+			if (stackOfSymbols.peak().equals(s[0])) {
+				System.out.println();
+			}
+			// Use Parse Table to find if any entry exists
+			else if (true) {
+
+			} else {
+				// error
+			}
+		}
+	}
 }
